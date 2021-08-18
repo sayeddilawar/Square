0aafb59ce258d0f606d9f74827d685b1a18d752a
Sayeddilawar <sayeddilawar631@gmail.com>
2021-08-18 17:25:04 +0530

    Requirement 2 : Implemented Perimeter of Square.

--- src/main/java/SquareMain.java
+++ src/main/java/SquareMain.java
@@ -8,4 +8,8 @@ public class SquareMain {
     {
         return side*side;
     }
+
+    public double perimeterOfSquare() {
+        return 4*side;
+    }
 }
--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -36,4 +36,38 @@ public class SquareTest
         SquareMain object = new SquareMain(-5.5);
         assertEquals(30.25,object.areaOfSquare());
     }
+
+    //Perimeter of Square
+    @DisplayName("Perimeter of Square with Positive side")
+    @Test
+    void toImplementPerimeterOfSquareWithPositiveSides()
+    {
+        SquareMain object = new SquareMain(10);
+        assertEquals(40,object.perimeterOfSquare());
+    }
+
+    @DisplayName("Perimeter of Square with Negative side")
+    @Test
+    void toImplementPerimeterOfSquareWithNegativeSides()
+    {
+        SquareMain object = new SquareMain(-10);
+        assertEquals(-40,object.perimeterOfSquare());
+    }
+
+    @DisplayName("Perimeter of Square with Positive Decimal side")
+    @Test
+    void toImplementPerimeterOfSquareWithPositiveDecimalSides()
+    {
+        SquareMain object = new SquareMain(2.5);
+        assertEquals(10,object.perimeterOfSquare());
+    }
+
+    @DisplayName("Perimeter of Square with Negative Decimal side")
+    @Test
+    void toImplementPerimeterOfSquareWithNegativeDecimalSides()
+    {
+        SquareMain object = new SquareMain(-2.5);
+        assertEquals(-10,object.perimeterOfSquare());
+    }
+    //End Perimeter Of Square
 }
