80d8e84f177ecd7288172360f92c11c59e793f56
Sayeddilawar <sayeddilawar631@gmail.com>
2021-08-18 17:14:14 +0530

    Requirement 1 : Implemented Area of Square.

--- src/main/java/SquareMain.java
+++ src/main/java/SquareMain.java
@@ -1,2 +1,11 @@
 public class SquareMain {
+    double side;
+    public SquareMain(double side)
+    {
+        this.side = side;
+    }
+    public double areaOfSquare()
+    {
+        return side*side;
+    }
 }
--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -1,2 +1,39 @@
-public class SquareTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareTest
+{
+
+    @DisplayName("Area of Square with Positive Sides")
+    @Test
+    void toImplementAreaOfSquareWithPositiveSides()
+    {
+        SquareMain object = new SquareMain(5);
+        assertEquals(25,object.areaOfSquare());
+    }
+
+    @DisplayName("Area of Square with Negative Sides")
+    @Test
+    void toImplementAreaOfSquareWithNegativeSides()
+    {
+        SquareMain object = new SquareMain(-5);
+        assertEquals(-25,object.areaOfSquare());
+    }
+
+    @DisplayName("Area of Square with Positive Decimal side")
+    @Test
+    void toImplementAreaOfSquareWithPositiveDecimalSides()
+    {
+        SquareMain object = new SquareMain(5.5);
+        assertEquals(30.25,object.areaOfSquare());
+    }
+
+    @DisplayName("Area of Square with Positive Decimal side")
+    @Test
+    void toImplementAreaOfSquareWithNegativeDecimalSides()
+    {
+        SquareMain object = new SquareMain(-5.5);
+        assertEquals(30.25,object.areaOfSquare());
+    }
 }
